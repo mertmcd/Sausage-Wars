4faeb5dfef6aa040f1c95ce879778dfdda91fcb1
Mert <merttdem@gmail.com>
2020-07-29 16:40:53 +0300

    init

--- src/game/playercontroller.js
+++ src/game/playercontroller.js
@@ -1,5 +1,3 @@
-import {Euler} from "three";
-
 export default class PlayerController {
   constructor(player) {
     this.player = player;
@@ -15,28 +13,7 @@ export default class PlayerController {
       let dx = 0.05 * (controls.mouseX - controls.downX);
       let dy = 0.05 * (controls.mouseY - controls.downY);
 
-      //  console.log("dx: " + dx);
-      // console.log("dy: " + dy);
-
-      // let ratio = dx / dy / 8;
-      // let ang = ratio * Math.PI;
-
-      // if (ratio > 0 && dx > 0) {
-      //   console.log("3");
-      // } else if (ratio > 0 && dx < 0) {
-      //   console.log("1");
-      //   ang += 180;
-      // } else if (ratio < 0 && dx > 0) {
-      //   console.log("2");
-      //   ang += 180;
-      // } else {
-      //   console.log("4");
-      // }
-
       let rota = Math.atan2(dx, dy);
-      // let rota = new Euler(0, ang, 0);
-
-      //  console.log(ang);
 
       if (dx > 7) dx = 7;
       else if (dx < -7) dx = -7;
@@ -46,7 +23,7 @@ export default class PlayerController {
 
       this.player.body.velocity.x = dx;
       this.player.body.velocity.z = dy;
-      this.player.rotation.y = rota; // copy(rota);
+      this.player.rotation.y = rota;
     }
 
     if (!isClicked) {
--- src/game/sausage.js
+++ src/game/sausage.js
@@ -5,7 +5,7 @@ import {Body, Box, Vec3} from "cannon";
 import PlayerController from "./playercontroller";
 
 export default class Sausage extends Object3D {
-  constructor(pos = new Vector3(), rot = new Euler()) {
+  constructor(pos, rot) {
     super();
 
     // Add sausage mesh and body
@@ -34,7 +34,7 @@ export default class Sausage extends Object3D {
     Globals.main.world.add(this.body);
 
     this.body.position.copy(this.position);
-    // this.body.quaternion.copy(this.quaternion);
+    //this.body.quaternion.copy(this.quaternion);
 
     this.updateWorldMatrix(true);
   }
@@ -47,7 +47,7 @@ export default class Sausage extends Object3D {
   update(delta) {
     this.body.position.y = 2.1;
     this.position.copy(this.body.position);
-    //this.quaternion.copy(this.body.quaternion);
+    // this.quaternion.copy(this.body.quaternion);
 
     if (!this.isAi) {
       this.controller.update(delta);
